06ce0f49c508d0d919316ebaae6b63d20133cdda
gustavosrosa <g.dsantosrosa@gmail.com>
2025-09-05 11:41:33 -0300

    PALI: BUGFIX: Correcao erro capslock

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,14 +5,14 @@ let resultadoP = document.querySelector('#resultado');
 btnVerificarPalindromo.addEventListener('click', () => {
     let palavraSemAcentos = inputPalavra.value.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
     let palavraSemCaracteres = palavraSemAcentos.replace(/[^a-zA-Z0-9]/g, '');
-    let palavraSemEspacos = palavraSemCaracteres.replaceAll(' ', '');
+    let palavraSemEspacos = palavraSemCaracteres.replaceAll(' ', '').toLowerCase();
 
-    const palavraInvertida = palavraSemEspacos.split('').reverse().join('')
+    const palavraInvertida = palavraSemEspacos.split('').reverse().join('').toLowerCase()
     console.log(palavraSemEspacos, palavraInvertida)
 
     if (palavraInvertida == palavraSemEspacos) {
-        resultadoP.textContent = `A palavra ${inputPalavra.value} é um palíndromo!`;
+        resultadoP.textContent = `${inputPalavra.value} é um palíndromo!`;
     } else {
-        resultadoP.textContent = `A palavra ${inputPalavra.value} não é um palíndromo!`;
+        resultadoP.textContent = `${inputPalavra.value} não é um palíndromo!`;
     }
 });
